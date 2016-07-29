3e7fa1d33bff600e91be5f976fc52e56c345a938
mwalugha <mwalughabura@gmail.com>
2016-07-29 06:10:54 +0300

    multiples and prime_factors solved

--- koan_labs/multiples.py
+++ koan_labs/multiples.py
@@ -1,4 +1,17 @@
 def multiple_sum():
-	#TODO: Your code goes here
-	return
-	
+	listOfNumbers = []
+	addition = 0
+
+	number = 0
+	while number < 1000:
+		if number % 3 == 0:
+			listOfNumbers.append(number)
+		elif number % 5 == 0:
+			listOfNumbers.append(number)
+
+		number+=1
+
+	for y in listOfNumbers:
+		addition+=y
+
+	return addition
